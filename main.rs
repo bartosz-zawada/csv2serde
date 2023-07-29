f5329d5572d0509bf32fd2513a1cb6555c6f481f
bartosz-zawada <zawada.bartosz@gmail.com>
2023-07-28 18:03:00 -0700

    Add CLI arguments with clap
    Every application should offer some flexibility. In this case, we want
    to be able to offer simple options like:
    - Specifying the name of the generated struct.
    - Changing the delimiter.
    - Outputting into a file.

--- main.rs
+++ main.rs
@@ -1,3 +1,5 @@
+use clap::{builder::ArgPredicate, Parser};
+use std::path::PathBuf;
 use thiserror::Error;
 
 #[derive(Error, Debug)]
@@ -5,6 +7,35 @@ enum C2SError {}
 
 type C2SResult<T> = Result<T, C2SError>;
 
+#[derive(Debug, Parser)]
+#[command(author, version, about)]
+pub struct Args {
+    /// File for which types will be generated.
+    /// If not provided, output will be sent to stdout.
+    file: PathBuf,
+
+    /// Name of the type, defaults to filename.
+    name: Option<String>,
+
+    /// File into which the types will be written.
+    #[arg(short = 'o', long)]
+    output: Option<PathBuf>,
+
+    /// If writing into a file, overwrite content rather than error out.
+    #[arg(short = 'f', long, requires_if(ArgPredicate::IsPresent, "output"))]
+    force: bool,
+
+    /// Character or string used as delimiter.
+    #[arg(short = 'd', long, default_value = ",")]
+    delimiter: String,
+
+    /// Number of rows to analyze for field type prediction.
+    /// A value of 0 means the entire file will be read and analyzed.
+    #[arg(short = 'l', long, default_value = "0")]
+    lines: Option<usize>,
+}
+
 fn main() -> C2SResult<()> {
+    let _args = Args::parse();
     Ok(())
 }
