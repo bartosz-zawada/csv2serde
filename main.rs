c105b42fb1601db08b4351faf508d25a64308abf
bartosz-zawada <zawada.bartosz@gmail.com>
2023-07-29 22:55:07 -0700

    Find valid parsers for each header
    This commit adds functionality to go over each field in each row, and
    process parse it with every known parser to find out which ones work.
    
    Whenever a parse fails for a field for a given header, we remove
    it from the valid pool of parsers for said header. This way, we avoid
    re-running already invalid parsers over the rest of the dataset for that
    column.
    
    It would be possible to also keep track of what field caused each parser
    to fail, for debugging purposes. Such feature might be added later on
    behind an CLI argument.

--- main.rs
+++ main.rs
@@ -60,6 +60,95 @@ fn parse_delimiter(arg: &str) -> Result<u8> {
     Ok(TryInto::<u8>::try_into(c).map_err(CantCastDelimiter)?)
 }
 
+#[derive(Clone, Debug)]
+enum TypeParser {
+    I8,
+    I16,
+    I32,
+    I64,
+    I128,
+    U8,
+    U16,
+    U32,
+    U64,
+    U128,
+    F32,
+    F64,
+    String,
+}
+
+impl TypeParser {
+    fn get_all() -> Vec<Self> {
+        vec![
+            TypeParser::I8,
+            TypeParser::I16,
+            TypeParser::I32,
+            TypeParser::I64,
+            TypeParser::I128,
+            TypeParser::U8,
+            TypeParser::U16,
+            TypeParser::U32,
+            TypeParser::U64,
+            TypeParser::U128,
+            TypeParser::F32,
+            TypeParser::F64,
+            TypeParser::String,
+        ]
+    }
+
+    fn can_parse(&self, field: &str) -> bool {
+        match self {
+            TypeParser::String => true,
+            TypeParser::U8 => field.parse::<u8>().is_ok(),
+            TypeParser::U16 => field.parse::<u16>().is_ok(),
+            TypeParser::U32 => field.parse::<u32>().is_ok(),
+            TypeParser::U64 => field.parse::<u64>().is_ok(),
+            TypeParser::U128 => field.parse::<u128>().is_ok(),
+            TypeParser::I8 => field.parse::<i8>().is_ok(),
+            TypeParser::I16 => field.parse::<i16>().is_ok(),
+            TypeParser::I32 => field.parse::<i32>().is_ok(),
+            TypeParser::I64 => field.parse::<i64>().is_ok(),
+            TypeParser::I128 => field.parse::<i128>().is_ok(),
+            TypeParser::F32 => field.parse::<f32>().is_ok(),
+            TypeParser::F64 => field.parse::<f64>().is_ok(),
+        }
+    }
+}
+
+#[derive(Clone, Debug)]
+struct Header {
+    #[allow(dead_code)]
+    name: String,
+    #[allow(dead_code)]
+    raw_name: String,
+    valid_parsers: Vec<TypeParser>,
+    optional: bool,
+    is_empty: bool,
+}
+
+impl From<&str> for Header {
+    fn from(name: &str) -> Self {
+        Header {
+            name: name.to_string(),
+            raw_name: name.to_string(),
+            valid_parsers: TypeParser::get_all(),
+            optional: false,
+            is_empty: true,
+        }
+    }
+}
+
+impl Header {
+    fn update_for(&mut self, field: &str) {
+        if field.is_empty() {
+            self.optional = true;
+        } else {
+            self.valid_parsers.retain(|parser| parser.can_parse(field));
+            self.is_empty = false;
+        }
+    }
+}
+
 fn main() -> Result<()> {
     let args = Args::parse();
 
@@ -71,14 +160,26 @@ fn main() -> Result<()> {
         .from_path(path)
         .map_err(|e| CantOpenReader(e, path_str))?;
 
-    let headers = reader.headers().map_err(CantParseHeaders)?;
-    println!("Headers: {:#?}", headers);
+    let mut headers: Vec<Header> = reader
+        .headers()
+        .map_err(CantParseHeaders)?
+        .iter()
+        .map(Header::from)
+        .collect();
 
     let lines = args.lines.unwrap_or(usize::MAX);
 
     for record in reader.records().take(lines) {
         let record = record.map_err(CantParseRecord)?;
-        println!("{:#?}", record);
+
+        for (i, field) in record.iter().enumerate() {
+            headers.get_mut(i).unwrap().update_for(field);
+        }
+    }
+
+    println!("Processed Headers:");
+    for h in headers {
+        println!("{:#?}", h);
     }
 
     Ok(())
